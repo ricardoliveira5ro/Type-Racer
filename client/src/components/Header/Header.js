cf4de6631469f68c964f8aefe520cb67e740828a
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2025-01-04 15:31:59 +0000

    Add lucide icons

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
+import { UserRound } from 'lucide-react';
+
 function Header() {
     return (
         <div className='w-100 px-10 py-5'>
             <div className='flex justify-between items-center bg-white rounded-md px-5 py-3'>
-                <span className='text-lg'>Type Racer</span>
-                <div className='flex gap-x-4'>
+                <Link to={'/home'} className='text-lg'>Type Racer</Link>
+                <div className='flex items-center gap-x-5'>
+                    <UserRound size={28} />
                     <button className='bg-[var(--dark)] text-white w-28 py-1 rounded-md'>Login</button>
                     <button className='bg-[var(--green)] w-28 py-1 rounded-md'>Sign Up</button>
                 </div>
