e8f3138fe7924fd7ec696b68c363388eb49ed2d6
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2025-02-28 00:14:13 +0000

    refactor wpm and calculate accuracy

--- client/src/components/Race/Race.js
+++ client/src/components/Race/Race.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect } from 'react';
 
 import { useQuoteTyping } from '../../hooks/useQuoteTyping';
 
@@ -11,10 +11,7 @@ import './Race.css'
 
 function Race({ title, players }) {
 
-    const { quote, typedWords, remainingWords, correctWordPart, wrongWordPart, currentWord, userInput, setUserInput, inputBgColor } = useQuoteTyping()
-    const userInputRef = useRef(null)
-
-    const [isRacing, setIsRacing] = useState(false);
+    const { isRacing, setIsRacing, quote, typedWords, remainingWords, correctWordPart, wrongWordPart, currentWord, userInput, setUserInput, userInputRef, inputBgColor, wpm, setElapsedTime } = useQuoteTyping()
 
     useEffect(() => {
         let timerId
@@ -26,19 +23,7 @@ function Race({ title, players }) {
         }, 15000);
     
         return () => clearTimeout(timerId)
-    }, [])
-
-    const [elapsedTime, setElapsedTime] = useState(0)
-    const [wpm, setWpm] = useState(0)
-
-    useEffect(() => {
-        if (!elapsedTime) return
-
-        const wordsTyped = typedWords?.length / 5 // Convert to words (5 chars = 1 word)
-        const timeMinutes = elapsedTime / 1000 / 60
-
-        setWpm(~~(wordsTyped / timeMinutes))
-    }, [typedWords]) // eslint-disable-line react-hooks/exhaustive-deps
+    }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
     return (
         <div className='flex flex-col gap-y-8'>
@@ -46,7 +31,7 @@ function Race({ title, players }) {
                 <img src={require('../../assets/images/race-flag.webp')} alt='Race flag' />
                 <p className='text-white text-2xl'>{title}</p>
             </div>
-            {!isRacing && <CountDownTimer />}
+            {(!isRacing && !typedWords) && <CountDownTimer />}
             <div className='flex flex-col justify-between items-center bg-white rounded-md px-5 py-3 w-full gap-y-4'>
                 {isRacing &&
                     <div className='flex justify-between w-full'>
@@ -79,8 +64,8 @@ function Race({ title, players }) {
                                 <span className="text-black"> {remainingWords}</span>
                             </p>
                         }
-                        <input onChange={(e) => setUserInput(e.target.value)} value={userInput} ref={userInputRef} placeholder='Type here' type='text' disabled
-                           className={`p-2 text-lg border-[1px] rounded-md border-gray-400 ${inputBgColor}`}>
+                        <input onChange={(e) => setUserInput(e.target.value)} value={userInput} ref={userInputRef} 
+                            placeholder='Type here' type='text' disabled className={`p-2 text-lg border-[1px] rounded-md border-gray-400 ${inputBgColor}`}>
                         </input>
                     </div>
                 </div>
--- client/src/hooks/useQuoteTyping.js
+++ client/src/hooks/useQuoteTyping.js
@@ -1,18 +1,28 @@
-import { useEffect, useMemo, useState } from "react"
+import { useEffect, useMemo, useRef, useState } from "react"
 import { useAppContext } from "../context/AppContext"
 
 
 export const useQuoteTyping = () => {
+
+    const [isRacing, setIsRacing] = useState(false);
  
     const { quotes } = useAppContext()
     const [quote, setQuote] = useState(null)
     const [quoteWords, setQuoteWords] = useState(null)
 
     const [userInput, setUserInput] = useState("")
+    const userInputRef = useRef(null)
     const [currentWord, setCurrentWord] = useState(null)
     const [wordIndex, setWordIndex] = useState(0)
 
     const [inputBgColor, setInputBgColor] = useState("bg-white");
+
+    const [elapsedTime, setElapsedTime] = useState(0);
+    const [wpm, setWpm] = useState(0);
+    const [accuracy, setAccuracy] = useState(0)
+
+    const prevUserInputRef = useRef("");
+    const [wrongCharactersCount, setWrongCharactersCount] = useState(0);
 
     const typedWords = useMemo(
         () => quoteWords?.slice(0, wordIndex).join(' '),
@@ -73,15 +83,42 @@ export const useQuoteTyping = () => {
     useEffect(() => {
         if (wordIndex === quoteWords?.length) {
             // End game
+            setIsRacing(false)
+            userInputRef.current.disabled = true
+
+            const totalCharacters = quote?.text.length
+            const totalCharactersTypes = totalCharacters + wrongCharactersCount
+            setAccuracy(~~((totalCharacters / totalCharactersTypes) * 100))
         }
-    }, [wordIndex, quoteWords])
+    }, [wordIndex, quoteWords, quote, wrongCharactersCount])
 
     useEffect(() => {
         (wrongWordPart || userInput.length > currentWord?.length) ? setInputBgColor("bg-red-300") :
                                                                     setInputBgColor("bg-white")
     }, [wrongWordPart, userInput, currentWord]);
 
+    useEffect(() => {
+        if (!elapsedTime || !isRacing) return
+
+        const wordsTyped = typedWords?.length / 5 // Convert to words (5 chars = 1 word)
+        const timeMinutes = elapsedTime / 1000 / 60
+
+        setWpm(~~(wordsTyped / timeMinutes))
+    }, [typedWords]) // eslint-disable-line react-hooks/exhaustive-deps
+
+    useEffect(() => {
+        const prevUserInput = prevUserInputRef.current;
+        
+        if (wrongWordPart && userInput.length > prevUserInput.length) {
+            setWrongCharactersCount(() => wrongCharactersCount + 1)
+        }
+
+        prevUserInputRef.current = userInput
+    }, [userInput, wrongWordPart, wrongCharactersCount])
+
     return {
+        isRacing,
+        setIsRacing,
         quote,
         typedWords,
         remainingWords,
@@ -90,6 +127,10 @@ export const useQuoteTyping = () => {
         currentWord,
         userInput,
         setUserInput,
-        inputBgColor
+        userInputRef,
+        inputBgColor,
+        wpm,
+        accuracy,
+        setElapsedTime,
     }
 }
