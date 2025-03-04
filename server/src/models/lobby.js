37a2c1a61f9361d772fe471bcf366554c182a7b6
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2025-03-04 22:59:12 +0000

    Add autogenerated code to Lobby model and validate array of players

--- server/src/models/lobby.js
+++ server/src/models/lobby.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose')
+const { randomBytes } = require('crypto')
 
 const User = require('./user');
 
 const lobbySchema = new mongoose.Schema({
-    players: [User],
+    players: {
+        type: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User' 
+        }],
+        validate: {
+            validator: (arr) => arr.length <= 4,
+            message: 'Max 4 players in a lobby'
+        }
+    },
     isPrivate: {
         type: Boolean,
         default: false
@@ -11,9 +21,21 @@ const lobbySchema = new mongoose.Schema({
     startCountDown: {
         type: Boolean,
         default: false
+    },
+    code: {
+        type: String
     }
 }, {
     timestamps: true
+})
+
+// Generate lobby code
+lobbySchema.pre('save', async function(next) {
+    if (this.isNew) {
+        this.code = randomBytes(4).toString('hex')
+    }
+
+    next()
 })
 
 const Lobby = mongoose.model('Lobby', lobbySchema)
