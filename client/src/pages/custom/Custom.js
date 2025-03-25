f803ef3184930b89a4faad2285947adf56e06337
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2025-03-25 22:58:14 +0000

    Auto assign party leader after leader left

--- client/src/pages/custom/Custom.js
+++ client/src/pages/custom/Custom.js
@@ -9,8 +9,6 @@ import Alert from "../../components/Alerts/Alert"
 import Race from "../../components/Race/Race"
 import CustomJoinLobby from "../../features/custom/JoinLobby"
 
-import { Copy } from "lucide-react"
-
 function Custom ({ socket }) {
 
     const { isActive, alertType, alertText, showAlert, dismissAlert } = useAlert()
@@ -19,7 +17,7 @@ function Custom ({ socket }) {
     const [code, setCode] = useState(null)
     const [create, setCreate] = useState(null)
 
-    const { isLoading: isLobbyLoading, lobby, startRace } = useCustomLobby(isUserLoading, userInfo, socket, create, code, showAlert)
+    const { isLoading: isLobbyLoading, lobby } = useCustomLobby(isUserLoading, userInfo, socket, create, code, showAlert)
 
     return (
         <div className="flex flex-col items-center gap-y-12 px-10 py-7">
@@ -28,23 +26,7 @@ function Custom ({ socket }) {
                 <Alert text={alertText} type={alertType} onDismissAlert={() => dismissAlert()} />
             }
             {!lobby && <CustomJoinLobby setCreate={setCreate} setCode={setCode} />}
-            {(!isLobbyLoading && lobby) &&
-                <>
-                    <Race socket={socket} mode={'Custom'} initialLobby={lobby} isGuest={userInfo ? false : true} />
-                    {(!lobby.startCountDown) &&
-                        <div className="flex items-center w-full gap-x-6">
-                            <div className='flex items-center gap-x-2 cursor-pointer' onClick={() => navigator.clipboard.writeText(lobby.code)}>
-                                <p className="text-white">Code:</p>
-                                <span className='text-[var(--green)] text-lg'>{lobby.code}</span>
-                                <Copy color="white" />
-                            </div>
-                            {socket.id === lobby.players[0].user &&
-                                <button onClick={() => startRace(socket.id, lobby.code)} className='px-8 py-1.5 rounded-md bg-[var(--green)]'>Start Race</button>
-                            }
-                        </div>
-                    }
-                </>
-            }
+            {(!isLobbyLoading && lobby) && <Race socket={socket} mode={'Custom'} initialLobby={lobby} isGuest={userInfo ? false : true} />}
         </div>
     )
 }
