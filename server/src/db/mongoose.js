a423fbd82dc44409bd5b585370a399cb40e53771
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2025-03-29 17:20:50 +0000

    test

--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -3,6 +3,8 @@ require("dotenv").config({ path: path.resolve(__dirname, '../config/.env') });
 
 const mongoose = require('mongoose')
 
+console.log("DB_CONNECTION_URL: ", process.env.DB_CONNECTION_URL);
+
 const DB_CONNECTION_URL = process.env.DB_CONNECTION_URL
 
 mongoose.connect(`${DB_CONNECTION_URL}`)
